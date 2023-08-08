0853c8da7ef8c0e40273ced1190c6e59cf336a30
JULIANO ROQUE VIEIRA <julianovieira@yahoo.com.br>
2023-08-07 21:27:27 -0300

    Input

--- resources/views/tipos/create.blade.php
+++ resources/views/tipos/create.blade.php
@@ -6,6 +6,11 @@
     </div>
     <div>
         <form action="" method="post">
+            <div class="mb-3">
+                <label for="descricao" class="form-label">Descrição</label>
+                <input type="text" class="form-control" id="descricao" 
+                placeholder="Exemplo: Cancelado" name="descricao">
+            </div>
             <div class="btn-group">
                 <button type="submit" class="btn btn-success">Salvar</button>
                 <button type="reset" class="btn btn-danger">Limpar</button>
