79820135648958aa26bd7bccbc26716748a95b29
JULIANO ROQUE VIEIRA <julianovieira@yahoo.com.br>
2023-08-07 21:08:25 -0300

    Botao Novo - Tipos

--- resources/views/tipos/index.blade.php
+++ resources/views/tipos/index.blade.php
@@ -1,4 +1,12 @@
 @extends('index')
 @section('content')
-<h1>Lista de Tipos</h1>
+<div>
+    <div>
+        <h1>Lista de Tipos</h1>
+    </div>
+    <div>
+        <a href=" {{ route('tipos.create') }}" 
+        class="btn btn-primary" role="button">Novo</a>
+    </div>
+</div>
 @endsection
