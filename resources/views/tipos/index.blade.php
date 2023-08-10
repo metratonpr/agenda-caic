c8c1097fa76d9d92bb1e5ca9df434da6fb0df982
JULIANO ROQUE VIEIRA <julianovieira@yahoo.com.br>
2023-08-09 21:05:44 -0300

    Table

--- resources/views/tipos/index.blade.php
+++ resources/views/tipos/index.blade.php
@@ -8,9 +8,30 @@
         <a href=" {{ route('tipos.create') }}" class="btn btn-primary" role="button">Novo</a>
     </div>
     <div>
-        @foreach($tipos_bichos as $t)
-        echo($t)
-        @endforeach
+    <div class="table-responsive">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Descrição</th>
+                    <th>Criado em:</th>
+                    <th>Atualizado em:</th>
+                    <th>Opções</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($tipos as $t)
+                <tr>
+                    <td> {{$t->id}} </td>
+                    <td> {{$t->descricao}} </td>
+                    <td> {{$t->created_at}} </td>
+                    <td> {{$t->updated_at}} </td>
+                    <td>Opções </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+        </div>
     </div>
 </div>
 @endsection
