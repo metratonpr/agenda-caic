39dd99a53ff159995383a5294a3f6805ca956a87
JULIANO ROQUE VIEIRA <julianovieira@yahoo.com.br>
2023-08-11 21:15:17 -0300

    Relacao Tarefa Tipo

--- app/Http/Requests/StoreTarefaRequest.php
+++ app/Http/Requests/StoreTarefaRequest.php
@@ -13,7 +13,7 @@ class StoreTarefaRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -25,6 +25,11 @@ class StoreTarefaRequest extends FormRequest
     {
         return [
             //
+            'data', 
+            'assunto', 
+            'descricao', 
+            'contato', 
+            'tipo_id'
         ];
     }
 }
